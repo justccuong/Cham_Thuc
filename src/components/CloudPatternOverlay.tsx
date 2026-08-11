e8d42a9c99f16289bdb4cc01a5f536fe128f2607
justccuong <caocuong7a2@gmail.com>
2026-08-11 14:33:06 +0700

    feat: Implement Programmatic Cloud Grid Generator in CloudPatternOverlay scattering 8 clouds along outer margins down full section height

--- src/components/CloudPatternOverlay.tsx
+++ src/components/CloudPatternOverlay.tsx
@@ -8,36 +8,47 @@ export function CloudPatternOverlay({ variant = "light" }: CloudPatternOverlayPr
     ? "filter invert sepia saturate-200 hue-rotate-30"
     : "filter sepia saturate-200 hue-rotate-320";
 
+  // Array of cloud image sources
+  const cloudImages = ["/patterns/cl1.png", "/patterns/cl2.png", "/patterns/cl3.png"];
+
+  // Pre-calculated scattered position coordinates across the section height
+  const cloudPositions: Array<{
+    top: string;
+    left?: string;
+    right?: string;
+    size: string;
+    opacity: string;
+    rotate: string;
+  }> = [
+    { top: "5%", left: "3%", size: "w-16", opacity: "opacity-15", rotate: "rotate-0" },
+    { top: "12%", right: "4%", size: "w-20", opacity: "opacity-20", rotate: "rotate-6" },
+    { top: "28%", left: "8%", size: "w-14", opacity: "opacity-10", rotate: "-rotate-12" },
+    { top: "35%", right: "12%", size: "w-16", opacity: "opacity-15", rotate: "rotate-0" },
+    { top: "52%", left: "4%", size: "w-20", opacity: "opacity-20", rotate: "rotate-12" },
+    { top: "65%", right: "6%", size: "w-14", opacity: "opacity-15", rotate: "-rotate-6" },
+    { top: "80%", left: "10%", size: "w-16", opacity: "opacity-15", rotate: "rotate-0" },
+    { top: "92%", right: "5%", size: "w-18", opacity: "opacity-20", rotate: "rotate-12" },
+  ];
+
   return (
     <div className="pointer-events-none absolute inset-0 z-0 select-none overflow-hidden" aria-hidden="true">
-      {/* Cloud 1: Top Right edge */}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/patterns/cl1.png"
-        alt=""
-        className={`absolute top-12 -right-6 w-20 md:w-28 opacity-15 ${filterClass}`}
-      />
-      {/* Cloud 2: Top Left edge */}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/patterns/cl2.png"
-        alt=""
-        className={`absolute top-8 -left-6 w-16 md:w-24 opacity-10 ${filterClass}`}
-      />
-      {/* Cloud 3: Bottom Right behind cards */}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/patterns/cl3.png"
-        alt=""
-        className={`absolute bottom-10 right-8 w-20 md:w-28 opacity-15 ${filterClass}`}
-      />
-      {/* Cloud 4: Bottom Left behind cards */}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/patterns/cl1.png"
-        alt=""
-        className={`absolute bottom-12 left-6 w-16 md:w-24 opacity-10 ${filterClass}`}
-      />
+      {cloudPositions.map((pos, index) => {
+        const imgSrc = cloudImages[index % cloudImages.length];
+        return (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            key={index}
+            src={imgSrc}
+            alt=""
+            style={{
+              top: pos.top,
+              left: pos.left,
+              right: pos.right,
+            }}
+            className={`absolute ${pos.size} ${pos.opacity} ${pos.rotate} ${filterClass} transition-all`}
+          />
+        );
+      })}
     </div>
   );
 }
